01f52f17e7398aab40fc2193282bf6b2a68106ac
Arang King <jykim0827@gmail.com>
2022-11-10 08:04:16 +0900

    Add Answers/DP05

---
+++ Answers/DP05/PasswordInfo.java
@@ -0,0 +1,25 @@
+public class PasswordInfo {
+    private String url;
+    private String userId;
+    private String password;
+
+    public PasswordInfo(String url, String id, String pw) {
+        this.url = url;
+        userId = id;
+        password = pw;
+    }
+
+    public String getUrl() { return url; }
+    public String getUserId() { return userId; }
+    public String getPassword() { return password; }
+
+    public void setUrl(String url) { this.url = url; }
+    public void setUserId(String id) { userId = id; }
+    public void setPassword(String pw) { password = pw; }
+
+    public String toString() {
+        return "\nUrl: " + url +
+                "\nUserId: " + userId +
+                "\nPassword: " + password;
+    }
+}
