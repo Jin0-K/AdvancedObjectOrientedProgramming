404ef38381513f5ab59e74896e028b0328f36d33
Arang King <jykim0827@gmail.com>
2022-12-02 19:03:14 +0900

    Add Answers/MP08code

---
+++ A09-ImageColour/src/Color.java
@@ -0,0 +1,13 @@
+public class Color {
+    int red, green, blue;
+
+    public Color(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+    }
+
+    int getRed() { return red; }
+    int getGreen() { return green; }
+    int getBlue() { return blue; }
+}
